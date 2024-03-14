a9f72aa966b4c300231912f1c7b0968119710fe7
dndmobilePub <git.publish01@dndmobile.co.kr>
2024-03-14 13:40:50 +0900

    BHY 약관 업데이트(페이지, 스와이퍼)

--- asset/js/ui.component.js
+++ asset/js/ui.component.js
@@ -1817,6 +1817,26 @@ var COMPONENT_UI = (function (cp, $) {
             });
         }
     };
+    /* swiper *//* BHY - 240314 추가된 것(삭제예정) */
+    cp.swiper = {
+        init() {
+            this.popSwiper();
+        },
+        popSwiper: function () {
+            $('.swip-rt').each(function () {
+                var swiper = new swiper(".swiper-wrapper",{
+                    slidesPerView:1,
+                    centeredSlides: true,
+                    loop : false,
+                    navigator:{
+                        nextEl:".swiper-button-next",
+                        prevEl:".swiper-button-prev",
+                    }
+                })
+
+            });
+        },
+    };
 
     cp.init = function () {
         cp.uaCheck.init(); // ver check
@@ -1828,6 +1848,7 @@ var COMPONENT_UI = (function (cp, $) {
         cp.selectBox.init();
         cp.tab.init();
         cp.accordion.init();
+        cp.swiper.init();
     };
 
     cp.init();
